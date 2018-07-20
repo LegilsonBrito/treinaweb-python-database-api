aeea53b20174ec50599e3df9d6d95b0576a5d295
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-07-20 09:58:07 -0300

    03 - Gerando cursores

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import MySQLdb
 
-db = MySQLdb.connect(user="root", passwd="root", db="ekodeadmin", host="localhost", port=3306)
+db = MySQLdb.connect(user="root", passwd="root", db="treinaweb_clientes", host="localhost", port=3306)
+cursor = db.cursor()
 print("Conexão realizada com sucesso")
 db.close()
