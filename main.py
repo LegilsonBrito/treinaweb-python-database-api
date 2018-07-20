ca5a453641b0f87274fc5d60f6620289317b87df
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-07-20 10:27:19 -0300

    04 - Lendo dados dos cursores

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import MySQLdb
 
 db = MySQLdb.connect(user="root", passwd="root", db="treinaweb_clientes", host="localhost", port=3306)
 cursor = db.cursor()
+cursor.execute("SELECT * FROM cliente")
+print(cursor.fetchmany(1))
 print("Conexão realizada com sucesso")
 db.close()
