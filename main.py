6d340f8a02cf8a35cc08d386f65f5674662614f0
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-07-24 14:53:01 -0300

    10 - SQL Injection

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
 import MySQLdb
 
-db = MySQLdb.connect(user="root", passwd="root", db="treinaweb_clientes", host="localhost", port=3306, autocommit=False)
+#Conectando ao banco de dados
+db = MySQLdb.connect(user="root", passwd="root", db="treinaweb_clientes", host="localhost", port=3306, autocommit=True)
+
+#Criando cursor
 cursor = db.cursor()
-cursor.execute("SELECT * FROM cliente")
-print(cursor.fetchall())
-print("Conexão realizada com sucesso")
-try:
-    db.begin()
-    cursor.execute("INSERT INTO cliente (nome, idade) VALUES ('Pedro', 25)")
-    cursor.execute("INSERT INTO cliente (nomes, idade) VALUES ('Augusto', 25)")
-    db.commit()
-except:
-    db.rollback()
-cursor.execute("SELECT * FROM cliente")
+#cursor.execute("SELECT * FROM cliente")
 #print(cursor.fetchall())
+
+#TRABALHANDO COM TRANSAÇÕES
+#try:
+#    db.begin()
+#    cursor.execute("INSERT INTO cliente (nome, idade) VALUES ('Pedro', 25)")
+#    cursor.execute("INSERT INTO cliente (nome, idade) VALUES ('Augusto', 25)")
+#    db.commit()
+#except:
+#    db.rollback()
+
+#CONSULTANDO DADOS
+#cursor.execute("SELECT * FROM cliente")
+#print(cursor.fetchall())
+
+#CONSULTANDO ÚLTIMO ID INSERIDO
 #print(cursor.lastrowid)
+
+#ATUALIZANDO DADOS
 #cursor.execute("UPDATE cliente SET nome='Ana' WHERE idcliente=2")
 #cursor.execute("SELECT * FROM cliente")
 #print(cursor.fetchall())
+
+#REMOVENDO DADOS
 #cursor.execute("DELETE FROM cliente WHERE idcliente=2")
 #cursor.execute("SELECT * FROM cliente")
 #print(cursor.fetchall())
+
+#INSERÇÃO COM PARÂMETROS SEM PREVENIR SQL INJECTION
+#nome = "Antônio"
+#cursor.execute(f"INSERT INTO cliente (nome, idade) VALUES ('{nome}', 25)")
+#cursor.execute("SELECT * FROM cliente")
+#print(cursor.fetchall())
+
+#SQL INJECTION
+#nome = "'Carlos' , idade = 80"
+#consulta = f"UPDATE cliente SET nome={nome} WHERE idcliente=13"
+#print(consulta)
+#cursor.execute(f"UPDATE cliente SET nome={nome} WHERE idcliente=13")
+#cursor.execute("SELECT * FROM cliente")
+#print(cursor.fetchall())
+
 db.close()
