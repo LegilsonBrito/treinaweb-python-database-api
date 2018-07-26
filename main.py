d3c46075f9aa77709ce75b59f59460cc95319043
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-07-26 15:48:25 -0300

    13 - Refatornando código

--- main.py
+++ main.py
@@ -1,66 +1,30 @@
-import MySQLdb
+import MySQLdb, cliente
 
 #Conectando ao banco de dados
 db = MySQLdb.connect(user="root", passwd="root", db="treinaweb_clientes", host="localhost", port=3306, autocommit=True)
 
 #Criando cursor
 cursor = db.cursor()
-#cursor.execute("SELECT * FROM cliente")
-#print(cursor.fetchall())
 
-#TRABALHANDO COM TRANSAÇÕES
-#try:
-#    db.begin()
-#    cursor.execute("INSERT INTO cliente (nome, idade) VALUES ('Pedro', 25)")
-#    cursor.execute("INSERT INTO cliente (nome, idade) VALUES ('Augusto', 25)")
-#    db.commit()
-#except:
-#    db.rollback()
+def listar_clientes():
+    cursor.execute("SELECT * FROM cliente")
+    print(cursor.fetchall())
 
-#CONSULTANDO DADOS
-#cursor.execute("SELECT * FROM cliente")
-#print(cursor.fetchall())
+def inserir_cliente(cliente):
+    cursor.execute("INSERT INTO cliente (nome, idade) VALUES (%s, %s)", (cliente.nome, cliente.idade))
 
-#CONSULTANDO ÚLTIMO ID INSERIDO
-#print(cursor.lastrowid)
+def editar_cliente(id_cliente, cliente):
+    cursor.execute("UPDATE cliente SET nome=%(nome)s, idade=%(idade)s WHERE idcliente=%(id_cliente)s",
+                   ({'nome': cliente.nome, 'idade': cliente.idade, 'id_cliente': id_cliente}))
 
-#ATUALIZANDO DADOS
-#cursor.execute("UPDATE cliente SET nome='Ana' WHERE idcliente=2")
-#cursor.execute("SELECT * FROM cliente")
-#print(cursor.fetchall())
+def remover_cliente(id_cliente):
+    cursor.execute("DELETE FROM cliente WHERE idcliente=%s", (id_cliente, ))
 
-#REMOVENDO DADOS
-#cursor.execute("DELETE FROM cliente WHERE idcliente=2")
-#cursor.execute("SELECT * FROM cliente")
-#print(cursor.fetchall())
+cliente = cliente.Cliente("João", 26)
 
-#INSERÇÃO COM PARÂMETROS SEM PREVENIR SQL INJECTION
-#nome = "Antônio"
-#cursor.execute(f"INSERT INTO cliente (nome, idade) VALUES ('{nome}', 25)")
-#cursor.execute("SELECT * FROM cliente")
-#print(cursor.fetchall())
-
-#SQL INJECTION
-#nome = "'Carlos' , idade = 80"
-#consulta = f"UPDATE cliente SET nome={nome} WHERE idcliente=13"
-#print(consulta)
-#cursor.execute(f"UPDATE cliente SET nome={nome} WHERE idcliente=13")
-#cursor.execute("SELECT * FROM cliente")
-#print(cursor.fetchall())
-
-#Parametrização de consultas
-#nome = "João"
-#idade = 40
-#cursor.execute("UPDATE cliente SET nome=%(nome)s, idade=%(idade)s WHERE idcliente=13", ({'nome': nome, 'idade': idade}))
-#cursor.execute("SELECT * FROM cliente")
-#print(cursor.fetchall())
-
-cursor.executemany("INSERT INTO cliente (nome, idade) VALUES (%s, %s)",
-                   (
-                       ('José', 50),
-                       ('Maria', 50),
-                       ('Pedro', 45),
-                       ('Fabio', 20)
-                   ))
+listar_clientes()
+inserir_cliente(cliente)
+editar_cliente(3, cliente)
+remover_cliente(6)
 
 db.close()
